2bdce1249e9b7aab581b1f4c63dea5c61a26eaa4
graham <donelsongrahamberger@gmail.com>
2020-01-13 20:39:05 -0800

    added imgs for apollo working on motion and wasd

--- Apollo.py
+++ Apollo.py
@@ -2,9 +2,13 @@
 class Apollo:
 
 
+
     def __init__(self, x, y, vel, health, armor):
         self.x = x
         self.y = y
         self.vel = vel
         self.health = health
         self.armor = armor
+
+    def draw(self):
+
