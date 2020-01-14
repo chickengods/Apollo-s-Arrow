82986c1933d153fc15f4bde3db344b7d80829541
graham <donelsongrahamberger@gmail.com>
2020-01-13 21:47:54 -0800

    first motoin of apollo

--- Apollo.py
+++ Apollo.py
@@ -6,15 +6,36 @@ class Apollo:
 
 
 
-    def __init__(self, x, y, vel, health, armor):
+    def __init__(self,x,y,vel,health,armor):
         self.x = x
         self.y = y
+        self.x_vel = 0
+        self.y_vel = 0
         self.vel = vel
         self.health = health
         self.armor = armor
         self.img = pygame.image.load(os.path.join("imgs","linkImg.png"))
 
-    def move(self, letter, vel):
+    def vel_change(self, letter, num):
+        if letter == 'w':
+            self.y_vel = self.y_vel - self.vel * num
+        elif letter == 's':
+            self.y_vel = self.y_vel + self.vel * num
+        elif letter == 'a':
+            self.x_vel = self.x_vel - self.vel * num
+        elif letter == 'd':
+            self.x_vel = self.x_vel + self.vel * num
+
+    def vel_zero(self, num):
+        if num == 1:
+            self.y_vel = 0
+        else:
+            self.x_vel = 0
+
+
+    def move(self):
+        self.x = self.x + self.x_vel
+        self.y = self.y + self.y_vel
 
 
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import Apollo
 
 WIN_WIDTH = 800
 WIN_HEIGHT = 800
@@ -9,24 +10,43 @@ clock = pygame.time.Clock()
 
 def draw_window(win, Apollo):
     Apollo.draw(win)
+    pygame.display.update()
 
-run = True
-while run:
-    clock.tick(30)
-    for event in pygame.event.get():
-        print(event)
-        if event.type == pygame.KEYDOWN():
-            if event.key == pygame.K_w:
-                # move
-            if else event.key == pygame.K_a:
-                # move
-            if else event.key == pygame.K_s:
+def main_game(apollo, win):
+    run = True
+    while run:
+        clock.tick(100)
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    apollo.vel_change('w',1)
+                if event.key == pygame.K_a:
+                    apollo.vel_change('a',1)
+                if event.key == pygame.K_s:
+                    apollo.vel_change('s',1)
+                if event.key == pygame.K_d:
+                    apollo.vel_change('d',1)
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:
+                    apollo.vel_change('w', -1)
+                if event.key == pygame.K_s:
+                    apollo.vel_change('s', -1)
+                if event.key == pygame.K_a:
+                    apollo.vel_change('a', -1)
+                if event.key == pygame.K_d:
+                    apollo.vel_change('d', -1)
 
-            if else event.key == pygame.K_d
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                quit()
+        apollo.move()
+        draw_window(win, apollo)
 
 
 
-        if event.type == pygame.QUIT:
-            run = False
-            pygame.quit()
-            quit()
+test = Apollo.Apollo(400,400,5,100,100)
+
+main_game(test, win)
+
