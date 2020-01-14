21a773832fb6ee7e36220446b849b0ef88f53cd7
graham <donelsongrahamberger@gmail.com>
2020-01-13 20:04:35 -0800

    the start of the apollo class

---
+++ Apollo.py
@@ -0,0 +1,10 @@
+
+class Apollo:
+
+
+    def __init__(self, x, y, vel, health, armor):
+        self.x = x
+        self.y = y
+        self.vel = vel
+        self.health = health
+        self.armor = armor
